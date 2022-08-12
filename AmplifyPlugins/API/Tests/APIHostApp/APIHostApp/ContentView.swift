0ee6b2dae89ebe2f7ac124fc86ac93fb81276938
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-08-12 11:30:26 -0400

    add SwiftUI call site

--- AmplifyPlugins/API/Tests/APIHostApp/APIHostApp/ContentView.swift
+++ AmplifyPlugins/API/Tests/APIHostApp/APIHostApp/ContentView.swift
@@ -7,17 +7,27 @@
 
 import SwiftUI
 import Amplify
+import AWSAPIPlugin
+import AWSPluginsCore
 
-
-class MockAPIPlugin: APICategoryGraphQLBehavior {
-    
-}
 class ContentViewModel: ObservableObject {
     
+    let apiPlugin: AWSAPIPlugin
+    init() {
+        apiPlugin = AWSAPIPlugin()
+    }
+    
+    func subscribe() async throws {
+        let operation = try await Amplify.API.subscribe(request: .subscription(of: Todo.self,
+                                                                               type: .onCreate))
+        // operation.sequence
+        
+        
+    }
 }
 
 struct ContentView: View {
-    @StateObject var vm = StateObject()
+    @StateObject var vm = ContentViewModel()
     
     var body: some View {
         VStack {
@@ -29,5 +39,45 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+public struct Todo: Model {
+    public let id: String
+    public var name: String
+    public var description: String?
+
+    public init(id: String = UUID().uuidString,
+                name: String,
+                description: String? = nil) {
+        self.id = id
+        self.name = name
+        self.description = description
+    }
+}
+
+extension Todo {
+  // MARK: - CodingKeys
+   public enum CodingKeys: String, ModelKey {
+    case id
+    case name
+    case description
+  }
+
+  public static let keys = CodingKeys.self
+  //  MARK: - ModelSchema
+
+  public static let schema = defineSchema { model in
+    let todo = Todo.keys
+
+    model.listPluralName = "Todos"
+    model.syncPluralName = "Todos"
+
+    model.fields(
+      .id(),
+      .field(todo.name, is: .required, ofType: .string),
+      .field(todo.description, is: .optional, ofType: .string))
     }
 }
